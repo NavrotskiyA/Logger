5ee9ddeeaf58d68d412d6eabf90cac513ba04732
Navrotskiy Alexandr <Navrotskiy.alex@gmail.com>
2021-10-19 22:03:30 +0300

    last

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 
  $writer = new \classes\Writer();
  $format = new \classes\Formater();
- $logger = new \classes\Logger($writer,$format,'logs');
+ $logger = new \classes\Logger($writer,$format,'log');
 
  $logger->log('debug','Some message for test', array("Some another info"," some array"));
 
